d3d91f10278dd5bb413ff7c3cfa6a4fcb55c9c06
maden51 <madenon123@gmail.com>
2022-08-29 12:20:52 +0300

    components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
+import List from './components/List';
+import Popup from './components/Popup';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo__Wrapper">
+      <Button 
+        className="todo_addButton" 
+        title="Добавить задачу" 
+        onClick={() => setOpen(true)}
+      />
+      <h1 className="todo_title">TODO_LIST</h1>
+      <List items={items} />
+      {open ? <Popup onClick={() => setOpen(false)} /> : null} 
     </div>
   );
 }
